1601909ece971a101f9e6b3f5d78a3896802fd1a
TarunZoro <tarp2605@gmail.com>
2024-07-04 00:23:12 +0530

    Information panel Enhancement

--- src/Components/InformationPlane.js
+++ src/Components/InformationPlane.js
@@ -1,8 +1,8 @@
-import { Box, Button, keyframes, Typography } from "@mui/material";
-import { useContext, useEffect, useState } from "react";
+import { Box, Button, Divider, Typography, keyframes } from "@mui/material";
+import { useState } from "react";
 import InformationDisplay from "../Constants/informationDisplay.json";
-import { AppContext } from "../Context/AppContext";
-import { setSelectionRange } from "@testing-library/user-event/dist/utils";
+import { Stepper, Step, StepLabel } from '@mui/material';
+import styled from "styled-components";
 
 const slideInLeft = keyframes`
   from {
@@ -22,122 +22,204 @@ const slideOutLeft = keyframes`
   }
 `;
 
-const slideInRight = keyframes`
-  from {
-    transform: translateX(100%);
-  }
-  to {
-    transform: translateX(0);
-  }
-`;
+export default function InformationPlane({ selectedPlanetState = 0 }) {
+  const [isOpen, setIsOpen] = useState(true);
+  const [activeStep, setActiveStep] = useState(null);
 
-const slideOutRight = keyframes`
-  from {
-    transform: translateX(0);
-  }
-  to {
-    transform: translateX(100%);
-  }
-`;
+  const togglePanels = () => {
+    setIsOpen(!isOpen);
+  };
 
-export default function InformationPlane({ selectedPlanetState=0 }) {
-    const [isOpen, setIsOpen] = useState(true);
+  const HoverStepLabel = styled(StepLabel)({
+    '&:hover': {
+      backgroundColor: 'rgba(33, 150, 243, 0.1)',
+      borderRadius: '4px',
+    },
+  });
 
+  return (
+    <>
+      <Button
+        sx={{
+          position: 'absolute',
+          bottom: '5%',
+          left: '50%',
+          transform: 'translateX(-50%)',
+          zIndex: 2,
+          backgroundColor: 'rgba(255, 255, 255, 0.8)',
+          color: 'black',
+          '&:hover': {
+            backgroundColor: 'rgba(255, 255, 255, 1)',
+          }
+        }}
+        onClick={togglePanels}
+      >
+        {isOpen ? 'Close Panels' : 'Open Panels'}
+      </Button>
 
-    const togglePanels = () => {
-        setIsOpen(!isOpen);
-    };
+      <Box
+        sx={{
+          position: 'absolute',
+          top: "10%",
+          left: 0,
+          height: '80%',
+          width: '30%',
+          backgroundColor: "rgba(0, 0, 0, 0.7)",
+          color: 'white',
+          zIndex: 1,
+          animation: `${isOpen ? slideInLeft : slideOutLeft} 1s ease-out`,
+          visibility: isOpen ? 'visible' : 'hidden',
+          opacity: isOpen ? 1 : 0,
+          transition: 'visibility 1s, opacity 1s',
+          overflowY: 'auto',
+          padding: '2rem',
+          marginLeft: '1rem',
+          '&::-webkit-scrollbar': {
+            width: '0.4em',
+          },
+          '&::-webkit-scrollbar-track': {
+            background: 'transparent',
+          },
+          '&::-webkit-scrollbar-thumb': {
+            background: 'rgba(255, 255, 255, 0.2)',
+          },
+          '&::-webkit-scrollbar-thumb:hover': {
+            background: 'rgba(255, 255, 255, 0.5)',
+          }
+        }}
+      >
+        <Typography variant="h3" gutterBottom sx={{ fontWeight: 'bold' }}>
+          {InformationDisplay.planets[selectedPlanetState].name}
+        </Typography>
+        <Divider sx={{ backgroundColor: 'white', marginBottom: '20px' }} />
 
+        <Typography paragraph>{InformationDisplay.planets[selectedPlanetState].basic_info.description}</Typography>
+        <Typography variant="h6" sx={{ fontWeight: 'bold', textTransform: 'uppercase' }}>
+          Basic Information
+        </Typography>
 
-    return (
-        <>
-            <Button
-                sx={{
-                    position: 'absolute',
-                    top: '5%',
-                    left: '50%',
-                    transform: 'translateX(-50%)',
-                    zIndex: 2
-                }}
-                onClick={togglePanels}
+        <Typography>
+          <Typography component="span" sx={{ fontWeight: 'bold' }}>
+            Radius:
+          </Typography>
+          {InformationDisplay.planets[selectedPlanetState].basic_info.radius}
+        </Typography>
+
+        <Typography>
+          <Typography component="span" sx={{ fontWeight: 'bold' }}>
+            Orbital Period:
+          </Typography>
+          {InformationDisplay.planets[selectedPlanetState].basic_info.orbital_period}
+        </Typography>
+
+        <Typography>
+          <Typography component="span" sx={{ fontWeight: 'bold' }}>
+            Rotation Period:
+          </Typography>
+          {InformationDisplay.planets[selectedPlanetState].basic_info.rotation_period}
+        </Typography>
+
+        <Typography>
+          <Typography component="span" sx={{ fontWeight: 'bold' }}>
+            Temperature:
+          </Typography>
+        </Typography>
+
+        {InformationDisplay.planets[selectedPlanetState].basic_info.temperature.average ? (
+          <ul>
+            <li>Average: {InformationDisplay.planets[selectedPlanetState].basic_info.temperature.average || "Not Available"}</li>
+            <li>Day: {InformationDisplay.planets[selectedPlanetState].basic_info.temperature.day || "Not Available"}</li>
+            <li>Night: {InformationDisplay.planets[selectedPlanetState].basic_info.temperature.night || "Not Available"}</li>
+          </ul>
+        ) : (
+          <Typography component="span">Average: {InformationDisplay.planets[selectedPlanetState].basic_info.temperature.core}</Typography>
+        )}
+
+        {selectedPlanetState !== 0 && (
+          <Typography>
+            <Typography component="span" sx={{ fontWeight: 'bold' }}>
+              Distance from Sun:
+            </Typography>
+            {InformationDisplay.planets[selectedPlanetState].basic_info.distance_from_sun}
+          </Typography>
+        )}
+
+        <Typography>
+          <Typography component="span" sx={{ fontWeight: 'bold' }}>
+            Gravity:
+          </Typography>
+          {InformationDisplay.planets[selectedPlanetState].basic_info.gravity}
+        </Typography>
+
+        <Typography>
+          <Typography component="span" sx={{ fontWeight: 'bold' }}>
+            Atmosphere:
+          </Typography>
+          {InformationDisplay.planets[selectedPlanetState].basic_info.atmosphere}
+        </Typography>
+
+        <Typography>
+          <Typography component="span" sx={{ fontWeight: 'bold' }}>
+            Surface:
+          </Typography>
+          {InformationDisplay.planets[selectedPlanetState].basic_info.surface}
+        </Typography>
+
+        <Typography>
+          <Typography component="span" sx={{ fontWeight: 'bold' }}>
+            Magnetic Field:
+          </Typography>
+          {InformationDisplay.planets[selectedPlanetState].basic_info.magnetic_field}
+        </Typography>
+
+        <Typography variant="h6" sx={{ fontWeight: 'bold', textTransform: 'uppercase' }}>
+          History Timeline
+        </Typography>
+
+        <Stepper orientation="vertical" nonLinear activeStep={activeStep}>
+          {InformationDisplay.planets[selectedPlanetState].history_timeline.map((event, index) => (
+            <Step
+              key={index}
+              onMouseEnter={() => {
+                setActiveStep(index);
+              }}
+              onMouseLeave={() => setActiveStep(null)}
             >
-                {isOpen ? 'Close Panels' : 'Open Panels'}
-            </Button>
+              <StepLabel>
+                <Typography sx={{
+                  userSelect: 'none',
+                  backgroundColor: activeStep === index ? 'rgba(69, 70, 71, 0.1)' : 'transparent',
+                  color: activeStep === index ? 'rgba(255, 255, 255, 0.8)' : 'rgba(255, 255, 255, 0.5)',
+                }}>{event}</Typography>
+              </StepLabel>
+            </Step>
+          ))}
+        </Stepper>
 
-            <Box
-                sx={{
-                    position: 'absolute',
-                    top: "10%",
-                    left: 0,
-                    height: '80%',
-                    width: '30%',
-                    backgroundColor: "rgba(0,0,0,0.5)",
-                    color: 'white',
-                    zIndex: 1,
-                    animation: `${isOpen ? slideInLeft : slideOutLeft} 1s ease-out`,
-                    display: isOpen ? 'block' : 'none',
-                    overflowY: 'auto',
-                    padding: '1rem',
-                    marginLeft: '1rem',
-                    //edit the scrollbar
-                    '&::-webkit-scrollbar': {
-                        width: '0.4em',
-                    },
-                    '&::-webkit-scrollbar-track': {
-                        background: 'transparent',
-                    },
-                    '&::-webkit-scrollbar-thumb': {
-                        background: 'rgba(0,0,0,0.2)',
-                    },
-                    '&::-webkit-scrollbar-thumb:hover': {
-                        background: 'rgba(0,0,0,0.5)',
-                    }
-                }}
-            >
-                <Typography variant="h4" gutterBottom>{InformationDisplay.planets[selectedPlanetState].name}</Typography>
-                <Typography variant="h6">Basic Information</Typography>
-                <Typography>Radius: {InformationDisplay.planets[selectedPlanetState].basic_info.radius}</Typography>
-                <Typography>Orbital Period: {InformationDisplay.planets[selectedPlanetState].basic_info.orbital_period}</Typography>
-                <Typography>Rotation Period: {InformationDisplay.planets[selectedPlanetState].basic_info.rotation_period}</Typography>
-                <Typography>Temperature:</Typography>
-                {
-                    InformationDisplay.planets[selectedPlanetState].basic_info.temperature.average ?
-                        <>
-                            <ul>
+        <Typography variant="h6" sx={{ fontWeight: 'bold', textTransform: 'uppercase' }}>
+          Moons
+        </Typography>
 
-                                <li>Average: {InformationDisplay.planets[selectedPlanetState].basic_info.temperature.average || "Not Available"}</li>
-                                <li>Day: {InformationDisplay.planets[selectedPlanetState].basic_info.temperature.day || "Not Available"}</li>
-                                <li>Night: {InformationDisplay.planets[selectedPlanetState].basic_info.temperature.night || "Not Available"}</li>
-                            </ul>
-                        </>
-                        :
-                        <li>Average: {InformationDisplay.planets[selectedPlanetState].basic_info.temperature.core}</li>
-                }
-                <Typography>Distance from Sun: {InformationDisplay.planets[selectedPlanetState].basic_info.distance_from_sun}</Typography>
-                <Typography>Gravity: {InformationDisplay.planets[selectedPlanetState].basic_info.gravity}</Typography>
-                <Typography>Atmosphere: {InformationDisplay.planets[selectedPlanetState].basic_info.atmosphere}</Typography>
-                <Typography>Surface: {InformationDisplay.planets[selectedPlanetState].basic_info.surface}</Typography>
-                <Typography>Magnetic Field: {InformationDisplay.planets[selectedPlanetState].basic_info.magnetic_field}</Typography>
-                <Typography paragraph>{InformationDisplay.planets[selectedPlanetState].basic_info.description}</Typography>
-                <Typography variant="h6">History Timeline</Typography>
-                <ul>
-                    {InformationDisplay.planets[selectedPlanetState].history_timeline.map((event, index) => (
-                        <li key={index}><Typography>{event}</Typography></li>
-                    ))}
-                </ul>
-                <Typography variant="h6">Moons</Typography>
-                <ul>
-                    {InformationDisplay.planets[selectedPlanetState].moons.length === 0 ? <li><Typography>None</Typography></li> : InformationDisplay.planets[selectedPlanetState].moons.map((moon, index) => (
-                        <li key={index}><Typography>{moon}</Typography></li>
-                    ))}
-                </ul>
-                <Typography variant="h6">Satellites</Typography>
-                <ul>
-                    {InformationDisplay.planets[selectedPlanetState].satellites.map((satellite, index) => (
-                        <li key={index}><Typography>{satellite}</Typography></li>
-                    ))}
-                </ul>
-            </Box>
-        </>
-    );
+        <ul>
+          {InformationDisplay.planets[selectedPlanetState].moons.length === 0 ? (
+            <li><Typography>None</Typography></li>
+          ) : (
+            InformationDisplay.planets[selectedPlanetState].moons.map((moon, index) => (
+              <li key={index}><Typography>{moon}</Typography></li>
+            ))
+          )}
+        </ul>
+
+        <Typography variant="h6" sx={{ fontWeight: 'bold', textTransform: 'uppercase' }}>
+          Satellites
+        </Typography>
+
+        <ul>
+          {InformationDisplay.planets[selectedPlanetState].satellites.map((satellite, index) => (
+            <li key={index}><Typography>{satellite}</Typography></li>
+          ))}
+        </ul>
+      </Box>
+    </>
+  );
 }
